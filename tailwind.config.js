02ff0251b154749ad51399090c6b7fe4a766ac45
harryg02 <94257270+harryg02@users.noreply.github.com>
2023-01-01 21:49:58 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        grayWhite: '#f8f8f8',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        LightGray: '#EEEEEE',
+        veryLightGray: '#F8F8F8',
+        solidBlack:'#000',
+        darkGray: '#777',
+        themeRed:'#be1d28',
+      },
+      fontFamily: {
+        display: ["Montserrat", "sans-serif"],
+      },
+      letterSpacing: {
+        wider: '.25em',
+        widest: '.35em',
+      },
+      fontSize: {
+        xs: '0.77rem',
+        sm: '0.9rem',
+
+      }
+    },
+  },
+  plugins: [],
+}
