39810683d74f85eca821ecb7fe7441db1b16aa55
harryg02 <94257270+harryg02@users.noreply.github.com>
2023-02-26 15:38:36 -0500

    finished new design but not yet implemented animation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 })
+
+var grammar = document.getElementById("an");
+var changevariable = document.getElementById("change");
+
+// setInterval(function() {
+//   if (changevariable.textContent === "DESIGNER") {
+//     changevariable.style.fontFamily = "Gochi Hand";
+//     changevariable.textContent = "ARTIST";
+//     grammar.textContent = "an";
+//   } 
+//   else if (changevariable.textContent === "ARTIST") {
+//     changevariable.style.fontFamily = "mono";
+//     changevariable.textContent = "FRONT-END DEVELOPER";
+//     grammar.textContent = "a";
+//   }
+//   else {
+//     changevariable.style.fontFamily = "Raleway";
+//     changevariable.textContent = "DESIGNER";
+//     grammar.textContent = "a";
+//   }
+// }, 3000);
+
