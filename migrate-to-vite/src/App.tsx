8aa1f1e9c48fab89a77fcb1e493da18dcdc6c290
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-27 14:34:09 -0400

    update 5 files and create 1 file

--- migrate-to-vite/src/App.tsx
+++ migrate-to-vite/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '/src/index.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React1</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
