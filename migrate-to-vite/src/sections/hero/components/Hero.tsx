16ad5c0f78b8818ce249fcfeaab96ec87e4a0639
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-27 20:58:38 -0400

    update Hero.tsx and Hero.css

--- migrate-to-vite/src/sections/hero/components/Hero.tsx
+++ migrate-to-vite/src/sections/hero/components/Hero.tsx
@@ -5,6 +5,7 @@ import intro3 from '../assets/intro-3.webm';
 import hero1 from '../assets/hero1.png';
 import hero2 from '../assets/hero2.png';
 import hero3 from '../assets/hero3.png';
+import '../styles/Hero.css';
 
 function Hero() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -68,6 +69,7 @@ function Hero() {
 
   return (
     <section id="hero" className="relative overflow-hidden bg-[#1C1E1F] min-h-[600px] max-h-[800px] h-[75vh] md:h-screen">
+      <div className="screen-lat" style={{ zIndex: 1 }}></div>
       <video id="myVideo1" autoPlay loop muted className="video-overlay absolute top-[15vh] left-[23vw] sm:left-[35%] lg:left-[35%] w-[40vw] object-scale-down block" style={{ height: '340px', minWidth: '350px' }}>
         <source src={intro1} type="video/mp4" />
         Your browser does not support the video tag.
@@ -83,6 +85,7 @@ function Hero() {
       <div id="imageOverlay1" className="absolute top-0 left-0 w-full h-full bg-cover bg-center block" style={{ backgroundImage: `url(${hero1})`, zIndex: 2 }}></div>
       <div id="imageOverlay2" className="absolute top-0 left-0 w-full h-full bg-cover bg-center hidden" style={{ backgroundImage: `url(${hero2})`, zIndex: 2 }}></div>
       <div id="imageOverlay3" className="absolute top-0 left-0 w-full h-full bg-cover bg-center hidden" style={{ backgroundImage: `url(${hero3})`, zIndex: 2 }}></div>
+      <div className="hero absolute top-0 left-0 w-full h-full bg-cover z-[600] bg-gradient-to-b from-[rgba(0,0,0,0.1)] to-[rgba(0,0,0,0.3)]"></div>
     </section>
   );
 }
