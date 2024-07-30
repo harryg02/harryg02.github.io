b103e0d2ecf1f986853ecb67b0feee02ddf25d50
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-07-30 14:35:47 -0400

    update script.js

--- js/script.js
+++ js/script.js
@@ -10,32 +10,11 @@ btn.addEventListener('click', () => {
 var grammar = document.getElementById("an");
 var changevariable = document.getElementById("change");
 
-// setInterval(function() {
-//   if (changevariable.textContent === "DESIGNER") {
-//     changevariable.style.fontFamily = "Gochi Hand";
-//     changevariable.textContent = "ARTIST";
-//     grammar.textContent = "an";
-//   } 
-//   else if (changevariable.textContent === "ARTIST") {
-//     changevariable.style.fontFamily = "mono";
-//     changevariable.textContent = "FRONT-END DEVELOPER";
-//     grammar.textContent = "a";
-//   }
-//   else {
-//     changevariable.style.fontFamily = "Raleway";
-//     changevariable.textContent = "DESIGNER";
-//     grammar.textContent = "a";
-//   }
-// }, 3000);
-
 const snippet1 = document.getElementById('snippet1');
 const snippet2 = document.getElementById('snippet2');
 const snippet3 = document.getElementById('snippet3');
 const snippets = [snippet1, snippet2, snippet3];
 
-// snippets[1].style.display = 'none';
-// snippets[2].style.display = 'none';
-// snippets[3].style.display = 'none';
 let currentSnippet = 0;
 
 function switchSnippet() {
