90a027211efbe1941b8aba00f486d5e5a8f2130c
harryg02 <94257270+harryg02@users.noreply.github.com>
2026-05-20 20:44:17 -0400

    update 1 file and create 1 file

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,5 @@
+import PrimaryButton from "./PrimaryButton";
+
 export default function Footer() {
   return (
     <footer className="bg-LightGray font-[urbanist]">
@@ -12,15 +14,14 @@ export default function Footer() {
         </div>
         <div className="basis-1/3 py-10 md:px-10 md:py-0">
           <h2 className="text-gray-900 font-[urbanist] font-extrabold text-lg leading-8">Contact Me</h2>
-          <a
-            href="mailto:tuharrygu@gmail.com"
-            className="flex flex-row justify-center items-center rounded-2xl w-[200px] px-5 py-4 my-2 bg-[#1E2128] hover:bg-[#222] shadow-md max-w-xs"
-          >
-            <div className="flex items-center mr-4">
-              <img src="/icons/mail.png" className="mr-0 w-6 h-6 object-contain" alt="icon" />
-              <span className="ml-2.5 text-white flex-shrink-1 font-semibold">Email Me</span>
-            </div>
-          </a>
+          <div className="my-3">
+            <PrimaryButton
+              href="mailto:tuharrygu@gmail.com"
+              icon={<img src="/icons/mail.png" className="w-4 h-4 object-contain" alt="" />}
+            >
+              Email Me
+            </PrimaryButton>
+          </div>
           <p className="my-2">
             <a className="hover:text-themeRed" href="https://www.linkedin.com/in/harrygu-ux/">
               <img className="inline h-8 m-[2px]" src="/icons/linkedin.svg" alt="LinkedIn Profile" />
